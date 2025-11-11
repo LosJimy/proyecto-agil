3355eb7296e5f858d6b3d845f82ed45aa21dc70a
LosJimy <112668599+LosJimy@users.noreply.github.com>
2025-11-11 15:08:22 -0300

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 //backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('Malla Academica UCN')
+  .setDescription('API para consultar malla academica con JWT (mejorar)')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- backend/src/malla/malla.controller.ts
+++ backend/src/malla/malla.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, UseGuards, Request, NotFoundException } from '@nestjs/common';
 import { MallaService } from './malla.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiTags, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('usuario')
+@ApiBearerAuth()
 @Controller('usuario')
 export class MallaController{
     constructor(private readonly mallaService: MallaService){}
 
     @UseGuards(JwtAuthGuard)
     @Get('malla')
+    @ApiResponse({ status: 200, description: 'Malla obtenida correctamente.' })
+    @ApiResponse({ status: 401, description: 'No autorizado.' })
     async obtenerMalla(@Request() req){
         const { carreras } = req.user;
 
